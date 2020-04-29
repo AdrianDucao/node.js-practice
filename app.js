9d7c2405bf21b1e68f75e7c03488f7dc4ef314c7
AdrianDucao <adrianducao@gmail.com>
2020-04-29 22:53:31 +0800

    organizing routes

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ sayHello('Ian'); */
 var express = require("express");
 var path = require("path");
 
-var routes = require("./routes");
+//var routes = require("./routes");
+
 var app = express();
 
 app.set("port",process.env.PORT || 3000);
@@ -15,7 +16,9 @@ app.set("port",process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(routes);
+//routes
+app.use("/", require("./routes/web"));
+app.use("/api", require("./routes/api"));
 
 app.listen(app.get("port"),function(){
     console.log("Server started on port " + app.get("port"));
