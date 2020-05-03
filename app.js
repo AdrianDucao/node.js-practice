af1e530195825f5eb0ebd7b1d6e646bcdf43328b
AdrianDucao <adrianducao@gmail.com>
2020-05-04 05:07:34 +0800

    added mongodb

--- app.js
+++ app.js
@@ -7,15 +7,26 @@ sayHello('Ian'); */
 var express = require("express");
 var path = require("path");
 var app = express();
+const mongoose = require('mongoose');
 
-app.set("port",process.env.PORT || 3000);
+//mongodb config
+const db = require('./config/keys').MongoURI;
 
+//connect to mongodb
+mongoose.connect(db,{ useNewUrlParser: true})
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+//ejs
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 //routes
 app.use("/", require("./routes/web"));
 app.use("/api", require("./routes/api"));
+
+//
+app.set("port",process.env.PORT || 3000);
 
 app.listen(app.get("port"),function(){
     console.log("Server started on port " + app.get("port"));
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports ={
+    MongoURI: 'mongodb+srv://adrianducao:A1d2r3i4a5n!1@cluster0-po5ym.mongodb.net/test?retryWrites=true&w=majority'
+}
